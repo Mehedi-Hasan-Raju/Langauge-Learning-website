73efbdd2dfa8c8c6c0becbe83120460782c9beb7
Mehedi-Hasan-Raju <tecmehedi7@gmail.com>
2026-09-21 23:53:24 +0600

    correction spealing

--- src/modules/learning/route/achievement/achievement.route.ts
+++ src/modules/learning/route/achievement/achievement.route.ts
@@ -1,19 +1,11 @@
 import { Router } from "express";
+// achievement route
+import {authenticate,} from "../../../../middlewares/auth.middleware";
 
-import {
-  authenticate,
-} from "../../../../middlewares/auth.middleware";
-
-import {
-  getUserAchievementsController,
-} from "../../controller/achievement/achievement.controller";
+import {getUserAchievementsController,} from "../../controller/achievement/achievement.controller";
 
 const router = Router();
 
-router.get(
-  "/",
-  authenticate,
-  getUserAchievementsController
-);
+router.get("/",authenticate,getUserAchievementsController);
 
 export default router;
